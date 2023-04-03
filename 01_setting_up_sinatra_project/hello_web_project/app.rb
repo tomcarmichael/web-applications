1c595d7234ed2551ad2c0697d895c185ab98b1bf
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-04-03 12:16:15 +0100

    Project setup, complete Phase 2 02 Exercise configuring routes in app.rb

---
+++ 01_setting_up_sinatra_project/hello_web_project/app.rb
@@ -0,0 +1,19 @@
+require "sinatra/base"
+require "sinatra/reloader"
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+      register Sinatra::Reloader
+  end
+
+  get '/' do
+    "testing... testing"
+  end
+
+  get '/hello' do
+    name = params[:name]
+    "Hello, #{name}!"
+  end
+end
