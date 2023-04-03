297e3e7392e2f02b56a247f41ef66070aef8b1ca
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-04-03 12:22:04 +0100

    Complete Phase 2 02 Callenge - route using POST

--- 01_setting_up_sinatra_project/hello_web_project/app.rb
+++ 01_setting_up_sinatra_project/hello_web_project/app.rb
@@ -16,4 +16,10 @@ class Application < Sinatra::Base
     name = params[:name]
     "Hello, #{name}!"
   end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    "Thanks #{name}, you sent this message: '#{message}'"
+  end
 end
