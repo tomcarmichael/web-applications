deb3c8ecee2b559d079fac23ad5aab9c272aa1eb
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-04-05 16:55:10 +0100

    Update DatabaseConnection for to use ENV variable for deployment

--- 05_test_driving_route_with_database/music_library_database_app/spec/lib/database_connection.rb
+++ 05_test_driving_route_with_database/music_library_database_app/spec/lib/database_connection.rb
@@ -11,12 +11,18 @@ class DatabaseConnection
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
   def self.connect
+      # If the environment variable (set by Render)
+    # is present, use this to open the connection.
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
     if ENV['ENV'] == 'test'
       database_name = 'music_library_test'
     else
       database_name = 'music_library'
     end
-    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+    @connection = PG.connect({ host: '127.0.0.1', dbname: music_library })
   end
 
   # This method executes an SQL query 
